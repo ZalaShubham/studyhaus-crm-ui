b5b2bfa29e52f001278013fe274ebf3e4db08ce7
ZalaShubham <shubhamzala1717@gmail.com>
2026-07-27 23:03:47 +0530

    revert: restore original dark mode UI per user preference

--- update_classes.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/class="seat /g, 'class="seat-card ');
-fs.writeFileSync('index.html', html);
-console.log('Classes replaced successfully.');
